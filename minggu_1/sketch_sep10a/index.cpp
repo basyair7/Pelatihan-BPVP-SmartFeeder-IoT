c68ca5b3f276ef27e8b91c704a4b9844d69a8e41
basyair7 <basyair.fathul@gmail.com>
2024-09-10 15:49:43 +0700

    add sketch_sep10a

---
+++ minggu_1/sketch_sep10a/index.cpp
@@ -0,0 +1,41 @@
+#include "index.h"
+#include "Arduino.h"
+
+void MyProgram::_setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < LED_COUNT; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
+}
+
+void MyProgram::test1() {
+  potensioValue   = analogRead(A0);
+  LEDBrightness1  = potensioValue / 4;
+  LEDBrightness2  = 255 - LEDBrightness1;
+  analogWrite(4, LEDBrightness1);
+  analogWrite(5, LEDBrightness2);
+
+  delay(10);
+}
+
+void MyProgram::test2() {
+  potensioValue   = analogRead(A0);
+  LEDBrightness1  = map(potensioValue, 0, 1023, 0, 255);
+  LEDBrightness2  = map(potensioValue, 0, 1023, 255, 0);
+  analogWrite(4, LEDBrightness1);
+  analogWrite(5, LEDBrightness2);
+  delay(10);
+}
+
+void MyProgram::test3() {
+  potensioValue = analogRead(A0);
+  int delayTime = map(potensioValue, 0, 1023, 100, 1000);
+
+  for (int i = 0; i < LED_COUNT; i++) {
+    for (int j = 0; j < LED_COUNT; j++) {
+      analogWrite(LED_PINS[j], (i == j) ? 255 : 0);
+    }
+
+    delay(delayTime);
+  }
+}
---
+++ minggu_1/sketch_sep10a/index.h
@@ -0,0 +1,29 @@
+#include "Arduino.h"
+
+#define public public:
+
+class MyProgram {
+  static const int LED_COUNT = 4;
+  int potensioValue = 0;
+  int LEDBrightness1, LEDBrightness2;
+  const int LED_PINS[LED_COUNT] = { 2, 3, 4, 5 };
+  void _setup();
+  void test1();
+  void test2();
+  void test3();
+  
+  public static MyProgram& instance() {
+    static MyProgram instance;
+    return instance;
+  }
+  
+  public static void main() {
+    instance()._setup();
+    
+    while(1) {  
+      instance().test1();
+      // instance().test2();
+      // instance().test3();
+    }
+  }
+};
