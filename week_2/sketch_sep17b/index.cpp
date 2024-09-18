a477004273d110948e1d582530a1e965903620b5
basyair7 <basyair.fathul@gmail.com>
2024-09-18 14:52:27 +0700

    modified sketch_sep17b

--- week_2/sketch_sep17b/index.cpp
+++ week_2/sketch_sep17b/index.cpp
@@ -44,34 +44,34 @@ void MyProgram::test1(void) {
     }
   }
 
-  // jalankan program led / pesan "selamat datang"
-  if (this->incoming) {
-    Serial.println(F("Selamat Datang"));
-    digitalWrite(this->LED_PINS[0], HIGH);
-    delay(2000);
-    digitalWrite(this->LED_PINS[0], LOW);
+  if (getDistance1 > LimitDistance || getDistance2 > LimitDistance) {
+    // jalankan program led / pesan "selamat datang"
+    if (this->incoming) {
+      Serial.println(F("Selamat Datang"));
+      digitalWrite(this->LED_PINS[0], HIGH);
+      delay(2000);
+      digitalWrite(this->LED_PINS[0], LOW);
 
-    // reset flag incoming beserta sensorDetected
-    this->incoming = this->sensorDetected1 = this->sensorDetected2 = false;
-  }
+      // reset flag incoming beserta sensorDetected
+      this->incoming = this->sensorDetected1 = this->sensorDetected2 = false;
+    }
 
-  // jalankan program led / pesan "selamat jalan"
-  if (this->outcoming) {
-    Serial.println(F("Selamat Jalan"));
-    digitalWrite(this->LED_PINS[1], HIGH);
-    delay(2000);
-    digitalWrite(this->LED_PINS[1], LOW);
+    // jalankan program led / pesan "selamat jalan"
+    if (this->outcoming) {
+      Serial.println(F("Selamat Jalan"));
+      digitalWrite(this->LED_PINS[1], HIGH);
+      delay(2000);
+      digitalWrite(this->LED_PINS[1], LOW);
 
-    // reset flag outcoming beserta sensorDetected
-    this->outcoming = this->sensorDetected1 = this->sensorDetected2 = false;
+      // reset flag outcoming beserta sensorDetected
+      this->outcoming = this->sensorDetected1 = this->sensorDetected2 = false;
+    }
   }
 
   delay(50);
 }
 
 float MyProgram::getDistance(uint8_t triggerPin, uint8_t echoPin) {
-  float _time, value;
-
   // Trigger Program
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
@@ -80,8 +80,5 @@ float MyProgram::getDistance(uint8_t triggerPin, uint8_t echoPin) {
   digitalWrite(triggerPin, LOW);
 
   // Echo program
-  _time = pulseIn(echoPin, HIGH);
-  value = _time * 0.034 / 2;
-
-  return value;
+  return pulseIn(echoPin, HIGH) * 0.034 / 2;
 }
